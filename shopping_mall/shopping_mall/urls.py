e24e8ea6a90c1f4a187188eb2c5aa12382a48808
Siwon  Kim <siwon@usf.edu>
2022-03-05 16:27:51 -0500

    Add Serializer for DRF

--- shopping_mall/shopping_mall/urls.py
+++ shopping_mall/shopping_mall/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from user.views import index, logout, RegisterView, LoginView
-from product.views import ProductList, ProductCreate, ProductDetail
+from product.views import ProductList, ProductCreate, ProductDetail, ProductListAPI
 from order.views import OrderCreate, OrderList
 
 urlpatterns = [
@@ -15,4 +15,5 @@ urlpatterns = [
     path('product/<int:pk>', ProductDetail.as_view()),
     path('order/', OrderList.as_view()),
     path('order/create/', OrderCreate.as_view()),
+    path('api/product/', ProductListAPI.as_view()),
 ]
