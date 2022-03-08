f348aa6b899102f8b47320af2881148c81d6efb4
Siwon  Kim <siwon@usf.edu>
2022-03-08 11:45:45 -0500

    Add Status with filter

--- shopping_mall/order/admin.py
+++ shopping_mall/order/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Order
 
+
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('user', 'product')
+    list_filter = ('status',)
+    list_display = ('user', 'product', 'styled_status')
+
+    def styled_status(self, obj):
+        if obj.status == 'refund':
+            return format_html(f'<span style="color:red">{obj.status}</span>')
+        if obj.status == 'confirmed':
+            return format_html(f'<span style="color:green">{obj.status}</span>')
+        return obj.status
+
+    styled_status.short_description = 'Status'
 
 admin.site.register(Order, OrderAdmin)
--- shopping_mall/order/models.py
+++ shopping_mall/order/models.py
@@ -4,6 +4,16 @@ class Order(models.Model):
     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE)
     quantity = models.IntegerField()
+    status = models.CharField(
+        choices = (
+            ('pending', 'pending'),
+            ('refund', 'refund'),
+            ('confirmed', 'confirmed'),
+            ('delivered', 'delivered'),
+        ),
+        default='pending', max_length=32
+    )
+    memo = models.TextField(null=True, blank=True)
     register_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
